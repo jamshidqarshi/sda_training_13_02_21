818670cbde5ad7373e738d41e1051d9a8d204e3a
Indrek Kaul <indrek.kaul@gmail.com>
2021-02-13 13:58:58 +0200

    Add CustomerRepository Class

--- pratical_project/src/main/java/persistence/CustomerRepository.java
+++ pratical_project/src/main/java/persistence/CustomerRepository.java
@@ -1,2 +1,44 @@
-package persistence;public class CustomerRepository {
+package persistence;
+
+import model.Transaction;
+import util.DBUtil;
+
+import javax.persistence.EntityManager;
+
+public class CustomerRepository {
+
+    private EntityManager entityManager;
+    //test
+
+    public CustomerRepository() {entityManager = DBUtil.getEntityManager();}
+
+    public void saveCustomer(Transaction t){
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(t);
+            entityManager.getTransaction().commit();
+        } catch (Exception e){
+            entityManager.getTransaction().rollback();
+        }
+    }
+
+    public void updateCustomer(Transaction t){
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(t);
+            entityManager.getTransaction().commit();
+        } catch (Exception e){
+            entityManager.getTransaction().rollback();
+        }
+    }
+
+    public void deleteCustomer(Transaction t){
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.remove(t);
+            entityManager.getTransaction().commit();
+        } catch (Exception e){
+            entityManager.getTransaction().rollback();
+        }
+    }
 }
