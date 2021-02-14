c29ec6b40841fca1d054265477943d5fcdcb5ecb
msalu <msalumaa@gmail.com>
2021-02-14 10:11:00 +0200

    CurrencyRepository with add method

--- pratical_project/src/main/java/persistence/CurrencyRepository.java
+++ pratical_project/src/main/java/persistence/CurrencyRepository.java
@@ -1,4 +1,25 @@
 package persistence;
 
+import model.CurrencyDailyPrice;
+import util.DBUtil;
+
+import javax.persistence.EntityManager;
+
 public class CurrencyRepository {
+    private EntityManager entityManager;
+
+
+    public CurrencyRepository() {
+        entityManager = DBUtil.getEntityManager();
+    }
+
+    public void save(CurrencyRepository currencyRepository){
+        try{
+            entityManager.getTransaction().begin();
+            entityManager.persist(currencyRepository);
+            entityManager.getTransaction().commit();
+        }catch (Exception e){
+            entityManager.getTransaction().rollback();
+        }
+    }
 }
